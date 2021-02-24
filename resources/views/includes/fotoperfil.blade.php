78b691169e845c5232b46ad3c5a5da03066f8c55
Moisem <al221910022@gmail.com>
2021-02-24 04:20:52 -0600

    Includes

--- resources/views/includes/fotoperfil.blade.php
+++ resources/views/includes/fotoperfil.blade.php
@@ -1,3 +1,5 @@
 @if(Auth::user()->image)
-    <img src="{{ route('user.image',['filename'=>Auth::user()->image]) }}" class="image">
+    <div class="container-image">
+        <img src="{{ route('user.image',['filename'=>Auth::user()->image]) }}" class="image">
+    </div>
 @endif
