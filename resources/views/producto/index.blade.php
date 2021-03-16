d90a84f65975f5ed43c19d3a31a793c02d95b91a
DuarteDc <al221910409@gmail.com>
2021-03-16 02:20:58 -0600

    Productos diseño

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -6,36 +6,34 @@
         <div class="col-md-8">
         @include('includes.message')
         @foreach($productos as $producto)
-            <div class="card pub_producto">
-                <div class="card-header">
-                @if($producto->user->image)
-                        <div class="container-image">
-                            <img src="{{ route('user.image',['filename'=>$producto->user->image]) }}" class="image">
-                        </div>
-                @endif
-                    <div class="nombre-user">
-                        {{ $producto->user->name.' '.$producto->user->apellidopaterno}}
-                    </div>
-                
+        <div class="pub_producto">
+            <div class="header-producto">
+                <div class="name-user">
+                    @if($producto->user->image)
+                    <img src="{{ route('user.image',['filename'=>$producto->user->image]) }}" class="img-profile">
+                    @endif
+                    {{ $producto->user->name.' '.$producto->user->apellidopaterno}}
                 </div>
-
-                <div class="card-body">
-                    <h6 class="card-subtitle mb-2 date">{{\FormatTime::LongTimeFilter($producto->created_at) }}</h6>
-                    <div class="img-content-productos"> 
-                        <img class="img-publicacion"src="{{route('producto.image',['filename'=>$producto->image])}}" alt="">
-                    </div>
-                    <div class="body-card-productos">
-                        <h5 class="card-title ">{{$producto->nombre}}</h5>
-                        <h6 class="card-subtitle mb-2 ">{{'Precio: $'.$producto->precio}}</h6>
-                        <h6 class="card-subtitle mb-2 ">{{$producto->estado}}</h6>
-                        <h6 class="card-subtitle mb-2 ">{{'Garantia: '.$producto->garantia}}</h6>
-                        <h6 class="card-subtitle mb-2 ">{{'Productos existentes: '.$producto->noexiencia}}</h6>
-                        <p class="card-text ">{{$producto->descripcion}}</p>
-                        <a href="{{route('producto.show', $producto)}}" class="btn btn-success">Ver Detalles</a> 
+                <div class="time"><h6>{{\FormatTime::LongTimeFilter($producto->created_at) }}</h6></div>
+            </div>
+            <div class="body-producto">
+                <div class="img-producto">
+                    <img class="img-publicacion"src="{{route('producto.image',['filename'=>$producto->image])}}" alt="">
+                </div>
+                <div class="desc-producto">
+                    <h3 class="my-3">{{$producto->nombre}}</h3>
+                    <h4>${{$producto->precio}}</h4>
+                    <hr>
+                    <p>Garantia: {{$producto->garantia}}</p>
+                    <p>Disponibilidad: {{$producto->noexistencia}}</p>
+                    <div class="footer-producto">
+                        <a href="{{route('producto.show', $producto)}}" class="btn btn-primary">Ver Detalles</a> 
                     </div>
                 </div>
             </div>
+        </div>
         @endforeach
+
         <div class="clearfix"></div>
         {{$productos->links()}}
         </div>
