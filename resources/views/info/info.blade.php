68a6d3fd7e0238172df1d467810082e7e53cf1dd
Moisem <al221910022@gmail.com>
2021-04-14 04:57:23 -0500

    info

--- resources/views/info/info.blade.php
+++ resources/views/info/info.blade.php
@@ -51,7 +51,7 @@
                     </div>
                 </div>
                 <div class="col-md-12 contacto content-center m-auto">
-                <form action="{{route('comentarios')}}" method="Post" class="form-contacto" >
+                <form action="{{route('contactanos')}}" method="Post" class="form-contacto" >
                     @csrf
                         <p class="h3 text-center"> Contactanos</p>
                         <div class="form-section ">
